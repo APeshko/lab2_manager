14d11b2a22238a762da496dc09e9222503f53237
APeshko <m2415803@edu.misis.ru>
2025-03-11 15:29:18 +0300

    Create library.cpp

---
+++ src/library.cpp
@@ -0,0 +1,17 @@
+#include "library.hpp"
+
+namespace template_library {
+
+    void EventScheduler::add_event(int time, const std::string& event) {
+        /*
+         * Realization
+         */
+    }
+
+    std::string EventScheduler::process_next() {
+        /*
+         * Realization
+         */
+        return ""; // Нет событий
+    }
+}
